7ab5182460236aa6f39113a5276b7ba75b28e3ee
denkalinovsky <denkalinovsky@gmail.com>
2021-02-18 18:01:54 +0300

    убрал переменную "_ajax", и и теперь получаю путь иначе, при подключении скриптов в function.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 /**
  * @subpackage testovoe
  * @since testovoe
@@ -6,66 +7,36 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="ru"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html lang="ru">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Тестовое задание</title>
-	<meta name="robots" content="noindex,nofollow">
-    <!-- <link rel="dns-prefetch" href="https://s.w.org/">
-            <script type="text/javascript">
-                window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/13.0.1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/#\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.6.1"}};
-                !function(e,a,t){var n,r,o,i=a.createElement("canvas"),p=i.getContext&&i.getContext("2d");function s(e,t){var a=String.fromCharCode;p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,e),0,0);e=i.toDataURL();return p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,t),0,0),e===i.toDataURL()}function c(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(o=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},r=0;r<o.length;r++)t.supports[o[r]]=function(e){if(!p||!p.fillText)return!1;switch(p.textBaseline="top",p.font="600 32px Arial",e){case"flag":return s([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])?!1:!s([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!s([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]);case"emoji":return!s([55357,56424,8205,55356,57212],[55357,56424,8203,55356,57212])}return!1}(o[r]),t.supports.everything=t.supports.everything&&t.supports[o[r]],"flag"!==o[r]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[o[r]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(n=t.source||{}).concatemoji?c(n.concatemoji):n.wpemoji&&n.twemoji&&(c(n.twemoji),c(n.wpemoji)))}(window,document,window._wpemojiSettings);
-            </script><script src="./assets/js/wp-emoji-release.min.js" type="text/javascript" defer=""></script> -->
-    <style type="text/css">
-    img.wp-smiley,
-    img.emoji {
-        display: inline !important;
-        border: none !important;
-        box-shadow: none !important;
-        height: 1em !important;
-        width: 1em !important;
-        margin: 0 .07em !important;
-        vertical-align: -0.1em !important;
-        background: none !important;
-        padding: 0 !important;
-    }
-    </style>
-        <!-- <link rel="stylesheet" id="wp-block-library-css" href="./assets/css/style.min.css" type="text/css" media="all"> -->
-        <!-- <link rel="stylesheet" id="test-app-css" href="./assets/css/app.css" type="text/css" media="all"> -->
-    <script type="text/javascript" id="jquery-core-js-extra">
-    // <![CDATA[ 
-    var _ajax = "<?php echo site_url()?>/wp-admin/admin-ajax.php";
-    // ]]>
-    </script>
-    <!-- <script type="text/javascript" src="./assets/js/jquery.min.js" id="jquery-core-js"></script> -->
-    <!-- <script type="text/javascript" src="./assets/js/jquery-migrate.min.js" id="jquery-migrate-js"></script> -->
-    <!-- <link rel="https://api.w.org/" href="#"><link rel="EditURI" type="application/rsd+xml" title="RSD" href="#"> -->
-    <!-- <link rel="wlwmanifest" type="application/wlwmanifest+xml" href="#">  -->
+    <meta name="robots" content="noindex,nofollow">
     <meta name="generator" content="WordPress 5.6.1">
-    <?php get_header();?>
+    <?php get_header(); ?>
 </head>
+
 <body class="home blog">
-<form class="decor" novalidate="">
-    <div class="form-left-decoration"></div>
-    <div class="form-right-decoration"></div>
-    <div class="circle"></div>
-    <div class="form-inner">
-        <h3>Добавить запись</h3>
-        <input type="text" name="user_login" placeholder="Username" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
-        <input type="email" name="user_email" placeholder="Email">
-        <input type="text" name="title" placeholder="Title">
-        <textarea name="text" placeholder="Сообщение..." rows="3"></textarea>
-        <div class="capthca-value-wrapper">
-            <span type="text "name="captcha-value" id="captcha-value-label" ></span>
+    <form class="decor" novalidate="">
+        <div class="form-left-decoration"></div>
+        <div class="form-right-decoration"></div>
+        <div class="circle"></div>
+        <div class="form-inner">
+            <h3>Добавить запись</h3>
+            <input type="text" name="user_login" placeholder="Username" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
+            <input type="email" name="user_email" placeholder="Email">
+            <input type="text" name="title" placeholder="Title">
+            <textarea name="text" placeholder="Сообщение..." rows="3"></textarea>
+            <div class="capthca-value-wrapper">
+                <span type="text " name="captcha-value" id="captcha-value-label"></span>
+            </div>
+            <input type="text" name="captcha-input" id="captcha-input" placeholder="Введите капчу...">
+            <label name="error-msg" id="error-msg">Капча введена неверно</label>
+            <button type="submit">Отправить</button>
         </div>
-        <input type="text" name="captcha-input" id="captcha-input" placeholder="Введите капчу...">
-        <label name="error-msg" id="error-msg">Капча введена неверно</label>
-        <button type="submit">Отправить</button>
-    </div>
-</form>
+    </form>
+    <?php include "footer.php"; ?>
+</body>
 
-
-<!-- <script type="text/javascript" src="./assets/js/app.js" id="app-js"></script> -->
-<!-- <script type="text/javascript" src="./assets/js/wp-embed.min.js" id="wp-embed-js"></script> -->
-
-<?php include "footer.php"; ?>
-
-</body></html>
+</html>
